c2da6d99acd99b556d44957ebbcbe8fc8f06f01e
Nikita Rusetsky <rusetscky@outlook.com>
2019-03-14 16:06:39 +0800

    Redesign

--- ResearchersWPF.UI/Properties/Resources.Designer.cs
+++ ResearchersWPF.UI/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace ResearchersWPF.UI.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap edit {
+            get {
+                object obj = ResourceManager.GetObject("edit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
